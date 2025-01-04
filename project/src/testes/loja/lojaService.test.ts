83335aa5a2fcdd081164032bff36fef454c73be1
Beatriz GDC <beatrizgdcevaio@gmail.com>
2025-01-04 19:41:04 -0300

    feat: create loja - TESTE UNITARIO

---
+++ project/src/testes/loja/lojaService.test.ts
@@ -0,0 +1,61 @@
+import { LojaService } from '../../models/lojas/service/lojaService';
+import { LojaRepository } from '../../models/lojas/repo/lojaRepo';
+import { CreateLojaDto } from '../../models/lojas/dtos/lojaDto';
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ServicoDeLogger } from '../../utils/logger/logger';
+
+describe('LojaService', () => {
+    let lojaService: LojaService;
+    let lojaModelMock: jest.Mock;
+
+    beforeEach(async () => {
+        lojaModelMock = jest.fn().mockImplementation(() => ({
+            save: jest.fn().mockResolvedValue({
+                _id: 'someId',
+                ...lojaData,
+            }),
+        }));
+
+        const module: TestingModule = await Test.createTestingModule({
+        providers: [
+            LojaService,
+            LojaRepository,
+            {
+                provide: getModelToken('Loja'),
+                useValue: lojaModelMock,
+            },
+            {
+                provide: ServicoDeLogger,
+                useValue: {
+                    log: jest.fn(),
+                    error: jest.fn(),
+                },
+            },
+        ],
+        }).compile();
+
+        lojaService = module.get<LojaService>(LojaService);
+    });
+
+    const lojaData: CreateLojaDto = {
+        lojaNome: 'MEOWBLE',
+        lojaTipo: 'PDV',
+        disponivelNoEstoque: true,
+        tempoDePreparo: 4,
+        latitude: '-37.0530322',
+        longitude: '-10.9833225',
+        codigoPostal: '49037-050',
+        numero: 800,
+        pais: 'Brazil',
+        lojaTelefone: '12345678',
+    };
+
+    it('deve criar uma nova loja', async () => {
+        const result = await lojaService.create(lojaData);
+
+        expect(result._id).toBe('someId');
+        expect(result.lojaNome).toBe(lojaData.lojaNome);
+        expect(lojaModelMock).toHaveBeenCalledWith(lojaData);
+    });
+});
