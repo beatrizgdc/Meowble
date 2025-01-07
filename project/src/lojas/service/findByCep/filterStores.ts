6c935aa41d517b2c3a36eb051b62603908d73e76
Beatriz GDC <beatrizgdcevaio@gmail.com>
2025-01-07 20:55:39 -0300

    feat: feat: response frete +50km

--- project/src/lojas/service/findByCep/filterStores.ts
+++ project/src/lojas/service/findByCep/filterStores.ts
@@ -2,9 +2,32 @@ import { LojaComDistancia } from '../../interface/lojaComDistanciaInterface';
 import { CorreiosService } from '../../../correios/correiosService';
 import { CorreiosDto } from '../../../correios/correiosDTO';
 import { ServicoDeLogger } from '../../../utils/logger/logger';
+import { formatCep } from './formatCep';
 
-function formatCep(cep: string): string {
-    return cep.replace(/-/g, '');
+function formatStoreData(
+    loja: LojaComDistancia,
+    frete: any,
+    distanciaKm: number
+) {
+    return {
+        name: loja.lojaNome,
+        postalCode: loja.codigoPostal,
+        type: loja.lojaTipo,
+        distance: `${distanciaKm.toFixed(0)} km`,
+        value: frete.map((item: any) => ({
+            prazo: item.prazo,
+            codProdutoAgencia: item.codProdutoAgencia,
+            price: item.precoAgencia,
+            description: item.urlTitulo,
+        })),
+        pins: {
+            position: {
+                lat: loja.latitude,
+                lng: loja.longitude,
+            },
+            title: loja.lojaNome,
+        },
+    };
 }
 
 export async function filterStores(
@@ -20,14 +43,15 @@ export async function filterStores(
         (loja) => loja.distanciaKm !== null && loja.distanciaKm < distanceLimit
     );
 
-    const lojasMaiorIgual50Km = lojas.filter(
+    const lojasMaiorIgual50Km: any[] = [];
+    const lojasFiltradas = lojas.filter(
         (loja) =>
             loja.distanciaKm !== null &&
             loja.distanciaKm >= distanceLimit &&
             loja.lojaTipo !== 'PDV'
     );
 
-    for (const loja of lojasMaiorIgual50Km) {
+    for (const loja of lojasFiltradas) {
         const correiosDto: CorreiosDto = {
             servico: '04014',
             cepOrigem: formatCep(loja.codigoPostal),
@@ -48,6 +72,9 @@ export async function filterStores(
         try {
             const frete = await correiosService.obterPrecosEPrazos(correiosDto);
             loja.frete = frete;
+            lojasMaiorIgual50Km.push(
+                formatStoreData(loja, frete, loja.distanciaKm!)
+            );
             console.log('sucesso!');
         } catch (error) {
             if (error instanceof Error) {
@@ -62,7 +89,7 @@ export async function filterStores(
     }
 
     lojasMenor50Km.sort((a, b) => a.distanciaKm! - b.distanciaKm!);
-    lojasMaiorIgual50Km.sort((a, b) => a.distanciaKm! - b.distanciaKm!);
+    lojasMaiorIgual50Km.sort((a, b) => a.distance - b.distance);
 
     return { lojasMenor50Km, lojasMaiorIgual50Km };
 }
